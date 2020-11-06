3e02787735b42eaeb96cad99fe07be3dfc1f6936
Jacob Berg <coffeesnakes@gmail.com>
2020-11-05 17:45:03 -0800

    scoring added on row fill

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ context.scale(20, 20)
 
 
 function arenaSweep () {
+  let rowCounter = 1;
   outer: for (let y = arena.length - 1; y > 0; --y) {
     for (let x = 0; x < arena[y].length; ++x) {
       if (arena[y][x] === 0) {
@@ -14,6 +15,8 @@ function arenaSweep () {
     const row = arena.splice(y, 1)[0].fill(0);
     arena.unshift(row);
     ++y
+    player.score += rowCount * 10;
+    rowCount *= 2;
   }
 }
 
@@ -135,6 +138,7 @@ function playerDrop() {
     merge(arena, player);
     playerReset();
     arenaSweep();
+    updateScore();
     player.pos.y = 0;
   }
   dropCounter = 0;
@@ -170,6 +174,8 @@ function playerReset () {
                  (player.matrix[0].length / 2 | 0);
   if (collision(arena, player)) {
     arena.forEach(row => row.fill(0));
+    player.score = 0;
+    updateScore();
   }
 }
 
@@ -205,11 +211,17 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+function updateScore () {
+  document.getElementById('score').innerText = player.score;
+}
+
 const arena = enterTheMatrix(12, 20)
 
 const player = {
   pos: { x: 5, y: 5 },
-  matrix: generatePiece('T'),
+  matrix: null,
+  score: 0,
+
 }
 
 const colors = [
@@ -239,4 +251,6 @@ document.addEventListener('keydown', event => {
   }
 })
 
+playerReset();
+updateScore();
 update();
