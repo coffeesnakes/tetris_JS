ac5fd5b2c20db3544d66de539dd6c6c8c650cc67
Jacob Berg <coffeesnakes@gmail.com>
2020-11-05 17:40:29 -0800

    arenaSweep added to clear filled rows

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const context = canvas.getContext('2d');
 context.scale(20, 20)
 
 
+function arenaSweep () {
+  outer: for (let y = arena.length - 1; y > 0; --y) {
+    for (let x = 0; x < arena[y].length; ++x) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    ++y
+  }
+}
+
 // const matrix = [
 //   [0, 0, 0],
 //   [1, 1, 1],
@@ -121,6 +134,7 @@ function playerDrop() {
     player.pos.y--;
     merge(arena, player);
     playerReset();
+    arenaSweep();
     player.pos.y = 0;
   }
   dropCounter = 0;
@@ -200,14 +214,14 @@ const player = {
 
 const colors = [
   null,
-  'red',
-  'blue',
-  'violet',
-  'green',
-  'purple',
-  'pink',
-  'orange'
-]
+  '#FF0D72',
+  '#0DC2FF',
+  '#0DFF72',
+  '#F538FF',
+  '#FF8E0D',
+  '#FFE138',
+  '#3877FF',
+];
 
 
 // event listeners accessing keyCode property moves position responsively.
